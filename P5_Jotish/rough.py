fbf7264c39939cab28dfe382ac9bb9074a0cfa99
Shree7676 <shreekantnandiyawar@gmail.com>
2023-03-02 11:06:00 +0530

    Python_developer

---
+++ P5_Jotish/rough.py
@@ -0,0 +1,7 @@
+planets = {1: ("surya", "mesh"), 2: ("chandra", "rushab"), 3: ("rahu", "mithun"),
+           4: ("brahaspati", "kark"), 5: ("none", "siha"), 6: ("budh", "kanya"),
+           7: ("shani", "tula"), 9: ("ketu", "dhanu"), 10: ("mangal", "makar"),
+           11: ("none", "kumba"), 12: ("shukra", "miin")}
+
+for x in planets:
+    print(planets[x][1])
